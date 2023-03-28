5b6d20423e6f28d56fbb7b1c725dd83d5812cacf
Asempere123123 <asempere@colegioaleman.net>
2023-03-28 18:12:14 +0200

    benchmarks

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use csv::WriterBuilder;
 
 mod base;
 
-const ITERACIONES: u32 = 1000;
+const ITERACIONES: u32 = 100000000;
 
 fn main() {
     //Variables del programa
@@ -10,6 +10,10 @@ fn main() {
     let mut tabla_partidas_jugadas: [[u32; 10000]; 4] = [[0; 10000]; 4];
     let mut tabla_winrate: [[f64; 10000]; 4] = [[0.0; 10000]; 4];
     let mut wrt = WriterBuilder::new().from_path("output.csv").expect("No se como, pero el programa ha petado.");
+
+    //Benchmark + Debug
+    let mut iteraciones_reales: u32 = 0;
+    let time = std::time::Instant::now();
 
     //Variables mazo
     let mut current_card: usize = 0;
@@ -46,6 +50,7 @@ fn main() {
 
         base::reset_player(&mut player1);
         base::reset_player(&mut player2);
+        iteraciones_reales += 1;
     }
 
     //calcular proporciones
@@ -67,4 +72,6 @@ fn main() {
         let content = tabla_winrate[i].iter().map(|&f| f.to_string()).collect::<Vec<_>>();
         wrt.write_record(&content).expect("No se como, pero el programa ha petado.");
     }
+
+    println!("Iteraciones realizadas: {}, en {}s", iteraciones_reales, time.elapsed().as_secs());
 }
